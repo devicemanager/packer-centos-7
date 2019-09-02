eb5946b2a24bf6769ec76596956bfe01616b1301
devicemanager <1844069+devicemanager@users.noreply.github.com>
2019-09-02 16:50:21 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,6 @@
 # Install vagrant key
 echo 'Install vagrant SSH key'
 mkdir -pm 700 /home/vagrant/.ssh
-wget --no-check-certificate https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub -O /home/vagrant/.ssh/authorized_keys
+wget --no-check-certificate https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub -O /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
