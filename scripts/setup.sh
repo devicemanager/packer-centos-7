e958a44529c92792d8c4efaa6cdb832ef3306119
devicemanager <1844069+devicemanager@users.noreply.github.com>
2019-09-03 10:27:47 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,3 +7,5 @@ mkdir -pm 700 /home/vagrant/.ssh
 wget --no-check-certificate https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub -O /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
+adduser rgeer -G wheel
+wget --no-check-certificate https://raw.githubusercontent.com/devicemanager/packer-centos-7/master/rgeer.pub -O /home/rgeer/.ssh/authorized_keys
